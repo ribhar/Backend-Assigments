daea3e90098843e315e9865d4a925163de9d83a8
Ritik <varribhar@gmail.com>
2022-06-13 16:36:03 +0530

    eval

---
+++ u5c4/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const {New,connection} = require("./db");
+
+const newsRouter=require("./route")
+
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+
+    res.send("hello")
+
+})
+app.use("/news",newsRouter)
+
+
+app.listen(8080,async ()=>{
+
+    try {
+
+        await connection;
+
+        console.log("Connected to db");
+
+    } catch {
+
+        console.log("Failed to connect")
+
+    }
+    
+    console.log("Server started at port 8080")
+})
